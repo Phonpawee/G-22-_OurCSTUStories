63d5b99ad069e02e79feaef32c9278fac7b04644
Suphanat <supanat.upa@dome.tu.ac.th>
2024-11-30 12:30:04 +0700

    The back button for friends has been fixed.

---
+++ Connect to a friend's page.js
@@ -0,0 +1,24 @@
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709610130.html';
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709650201.html';
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709650490.html';
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709650649.html';
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709650698.html';
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    window.location.href = '6709650714.html';
+});
